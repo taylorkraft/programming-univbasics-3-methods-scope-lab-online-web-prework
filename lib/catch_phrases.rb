b2ac50f5828673496b6a50e33b0896c777d00b65
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-19 00:34:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,10 @@
-def mario
+def link
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
 
+def any_phrase
+  phrase = "Curiosity killed the cat!"
+  puts status
+end
